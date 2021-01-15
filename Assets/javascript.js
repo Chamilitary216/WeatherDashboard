aed68584fbd279d49856e0bc02569c5dba66ec6d
Chris Curry <Chamilitary216@hotmail.com>
2021-01-15 02:12:09 -0500

    Switched method for API call

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,9 +1,58 @@
-// API Key
-var APIKey = "fc22f9676ccf5c3658a3938bde4d0e22";
+$(document).ready(function(){
 
-//variable city data to local storage
-var city = localStorage.getItem("lastResult");
+    $("#search-button").click(function(){
+        var city = $("search-value").val();
 
-//Query URL
+        if(city != ""){
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + APIKey + city;
+            $.ajax({
+
+                // API call for city and imerial units
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=24d12d7e66703d2d1e26e24ab8117ec4",
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data){
+                    console.log(data);
+
+                }
+                
+
+
+
+            });
+
+        }else{
+            $("#error").html("field cannot be empty");
+        }
+    });
+
+
+});
+
+// // API Key
+// var APIKey = "fc22f9676ccf5c3658a3938bde4d0e22";
+
+// //variable city data to local storage
+// var city = localStorage.getItem("lastResult");
+
+// //Query URL
+
+// var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + APIKey + city;
+
+// // 
+
+// // Current date
+
+// var currentDate = moment().format("dddd, MMMM Do");
+
+// function insertCurrentDay() {
+//     $(".current-date").text(currentDay);
+// };
+
+// //Temp conversion 
+// var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+// $(".tempF").text("Temperature: " + Math.round(tempF) + " °F");
+
+// var lat = response.coord.lat;
+// var lon = response.coord.lon;
+// var queryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + lat + "&lon=" + lon + APIKey;
